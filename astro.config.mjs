8d0977fd6a1c4acc65d4cf8006c6fd698deefcbf
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2024-03-09 08:10:48 -0300

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: "https://drinkoffee.com",
+  integrations: [sitemap(), react(), tailwind()],
+});
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "Drinkoffee | Isaac Miranda";
+export const SITE_DESCRIPTION = "A personal blog by Isaac Miranda";
